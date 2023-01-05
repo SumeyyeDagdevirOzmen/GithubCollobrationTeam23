efba9710a70dd90e01ee0448e59ac7d1c4b1b665
Suheyla Yanmaz <tdesosctle@gmail>
2023-01-05 16:50:32 +0100

    Suheyla branchi eklendi

--- src/Ozellik.java
+++ src/Ozellik.java
@@ -3,5 +3,6 @@ public class Ozellik {
 
         System.out.println("Sumeyye Branc'inde ozellik class'i olusturuldu");
         System.out.println("ayhan brachinde Ozellik classinda degisiklik yaptim");
+        System.out.println("Suheyla branch' inde Ozellik class' inda degisiklik yaptim");
     }
 }
