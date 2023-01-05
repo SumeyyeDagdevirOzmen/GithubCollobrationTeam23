9c316cd5c6359a29a2ba68d13fdd77172c8c1d0c
profensor <ayhansezerim@gmail.com>
2023-01-05 14:55:08 +0100

    main branch degisiklik

--- src/Ozellik.java
+++ src/Ozellik.java
@@ -1,5 +1,7 @@
 public class Ozellik {
     public static void main(String[] args) {
         System.out.println("Sumeyye Branc'inde ozellik class'i olusturuldu");
+        System.out.println("main branch degisiklik 1.satir");
+        System.out.println("main branch degisiklik 2.satir");
     }
 }
