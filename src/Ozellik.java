549e6e41a0a766fbae106ee45962545cf7f84d76
profensor <ayhansezerim@gmail.com>
2023-01-05 14:52:39 +0100

    ayhan branchi 2.degisiklik

--- src/Ozellik.java
+++ src/Ozellik.java
@@ -1,5 +1,7 @@
 public class Ozellik {
     public static void main(String[] args) {
+
         System.out.println("Sumeyye Branc'inde ozellik class'i olusturuldu");
+        System.out.println("ayhan brachinde Ozellik classinda degisiklik yaptim");
     }
 }
