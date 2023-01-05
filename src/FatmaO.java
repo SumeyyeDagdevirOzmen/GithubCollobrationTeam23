936879790da19e499e9f025d836078be3597e460
Fatma Ozmen <fatmaozmen.be@gmail.com>
2023-01-05 23:41:06 +0100

    ucuncu

--- src/FatmaO.java
+++ src/FatmaO.java
@@ -2,5 +2,6 @@ public class FatmaO {
     public static void main(String[] args) {
         System.out.println("ilk");
         System.out.println("ikinci");
+        System.out.println("ucuncu");
     }
 }
