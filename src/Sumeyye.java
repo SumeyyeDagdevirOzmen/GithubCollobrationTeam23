1f68ba275474882c01cb15146115896115830e28
Sumeyye <sumeyyedagdevirozmen@gmail.com>
2023-01-06 22:21:26 +0000

    v3

--- src/Sumeyye.java
+++ src/Sumeyye.java
@@ -2,5 +2,7 @@ public class Sumeyye {
     public static void main(String[] args) {
 
         System.out.println("Sumeyye master Branch'i olusturuldu");
+        System.out.println("2.degisiklik yapildi");
+        System.out.println("3.degisiklik");
     }
 }
