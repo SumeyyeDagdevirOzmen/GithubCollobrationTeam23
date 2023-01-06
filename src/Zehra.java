2393a2b83eccc652da11229f188e2ca17f39d20b
zehraysl <uysalzehraa@gmail.com>
2023-01-06 13:25:53 +0100

    V2

--- src/Zehra.java
+++ src/Zehra.java
@@ -2,7 +2,7 @@ public class Zehra {
     public static void main(String[] args) {
         System.out.println("Zehra branch'i olustu");
 
-
+        System.out.println("2.deneme" );
         System.out.println("deneme");
     }
 }
